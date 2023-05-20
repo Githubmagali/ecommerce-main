8590e9407f7e925ba22b65b4836e87706512d506
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-05-20 19:38:00 -0300

    cambios

--- app/Http/Livewire/Admin/AdminAddCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminAddCategoryComponent.php
@@ -38,6 +38,7 @@ class AdminAddCategoryComponent extends Component
     }
     public function render()
     {
+        
         return view('livewire.admin.admin-add-category-component');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Controllers\ProfileController;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\Admin\AdminCategoriesComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\WishlistComponent;
 use App\Http\Livewire\WishListIconComponent;
 
@@ -57,6 +60,10 @@ Route::middleware(['auth', 'authadmin'])->group(function(){
 Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
 Route::get('/admin/categories', AdminCategoriesComponent::class)->name('admin.categories');
 Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.category.add');
+Route::get('/admin/category/edit/{category_id}', AdminEditCategoryComponent::class)->name('admin.category.edit');
+Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
+Route::get('/admin/product/add', AdminAddProductComponent::class)->name('admin.product.add');
+
 });
 
 
